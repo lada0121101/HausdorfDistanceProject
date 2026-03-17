c6dee0308e9588a845d623a31cfd5d0d0b3ce526
lada0121101 <82163558+lada0121101@users.noreply.github.com>
2026-03-17 17:54:56 +0500

    Добавьте файлы проекта.

---
+++ BitArrayExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+namespace HausdorfDistanceProject
+{
+    internal class BitArrayExtension
+    {
+        public BitArrayExtension(BitArray bits , int length) 
+        {
+            this.bits = bits;
+            this.length = length;
+        }
+        private int length;
+        private BitArray bits;
+        public bool this[int x, int y] { get => bits[x * length + y];set => bits[x * length + y] = value; }
+    }
+}
---
+++ K2Tree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HausdorfDistanceProject
+{
+    internal class K2TreeNode
+    {
+        public int value;
+        public int minx;
+        public int maxx;
+        public int miny;
+        public int maxy;
+        public LinkedList<K2TreeNode> childs = new LinkedList<K2TreeNode>();
+    }
+}
