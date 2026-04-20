4e768a40b87008aa5f4c5b05caa09c7ba2d92ef1
lada0121101 <82163558+lada0121101@users.noreply.github.com>
2026-04-21 01:43:18 +0500

    Реализованы модифицированные алгоритмы HDKP1 и HDKP2

--- Algorithms.cs
+++ Algorithms.cs
@@ -21,7 +21,58 @@ namespace HausdorfDistanceProject
             }
             return cmax;
         }
+        public double MHDKP1(MK2TreeNode a, MK2TreeNode b)
+        {
+            var cmax = 0.0;
+            foreach (var p in a)
+            {
+                var minNN = Dist(p, pNN);
+                if (minNN > cmax)
+                    cmax = MNNMax(b, p, cmax, minNN);
+            }
+            return cmax;
+        }
 
+        private double MNNMax(MK2TreeNode b, (int x, int y) p, double cmax, double minNN)
+        {
+            var pQ = new PriorityQueue<K2TreeNode, double>();
+            var d = MaxDist(p, b);
+            pQ.Enqueue(b, d);
+            while (pQ.Count != 0)
+            {
+                K2TreeNode node;
+                double dist;
+                var suceess = pQ.TryDequeue(out node, out dist);
+                if (node.value == 1)
+                {
+                    if (dist <= cmax)
+                        return cmax;
+                    foreach (var child in node.childs)
+                    {
+                        if (child.childs.Count != 0)
+                        {
+                            var childMinDist = MinDist(p, child);
+                            if (childMinDist < minNN)
+                            {
+                                var childMaxDist = MaxDist(p, child);
+                                pQ.Enqueue(child, childMaxDist);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if (dist <= cmax)
+                        return cmax;
+                    if (dist < minNN)
+                    {
+                        minNN = dist;
+                        pNN = (node.minx, node.miny);
+                    }
+                }
+            }
+            return minNN;
+        }
         private double NNMax(K2TreeNode b, (int x, int y) p, double cmax, double minNN)
         {
             var pQ = new PriorityQueue<K2TreeNode,double>();
@@ -67,7 +118,17 @@ namespace HausdorfDistanceProject
         {
             return node.minx == node.maxx && node.value == 1;
         }
+
         public double MaxDist((int x, int y) p, K2TreeNode node) 
+        {
+            var maxDx = 0;
+            var maxDy = 0;
+            maxDx = p.x > (node.maxx + node.minx) / 2.0 ? p.x - node.minx : node.maxx - p.x;
+            maxDy = p.y > (node.maxy + node.miny) / 2.0 ? p.y - node.miny : node.maxy - p.y;
+            return Math.Sqrt(maxDx * maxDx + maxDy * maxDy);
+        }
+
+        public double MaxDist((int x, int y) p, MK2TreeNode node)
         {
             var maxDx = 0;
             var maxDy = 0;
---
+++ MK2TreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HausdorfDistanceProject
+{
+    internal class MK2TreeNode
+    {
+        public int value;
+        public int minx;
+        public int maxx;
+        public int miny;
+        public int maxy;
+        public LinkedList<K2TreeNode> childs = new LinkedList<K2TreeNode>();
+        public IEnumerator<(int, int)> GetEnumerator()
+        {
+            if (minx == maxx && value == 1)
+                yield return (minx, miny);
+            else
+            {
+                foreach (var child in childs)
+                {
+                    foreach (var point in child)
+                        yield return point;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
